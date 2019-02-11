a8030a16f10ba838d348787aacb381135bbaeb3a
Brando Meniconi <amministrazione@silverbackstudio.it>
2019-02-11 17:47:42 +0000

    add support for custom classes and formSubmitted event

--- src/forms.js
+++ src/forms.js
@@ -7,12 +7,6 @@ window.dataLayer = window.dataLayer || [];
 
 (function($){
 
-    var applyCss = function($form){ 
-	    var cssFile = '<link rel="stylesheet" href="' + hubspotFormThemeCss + '" type="text/css" />';
-	    
-	    $(cssFile).appendTo( $form.closest('html').find('head') );
-	};
-
     $('.wp-block-hubspot-form').each(function(){
 
         var $form_container = $(this);
@@ -31,26 +25,38 @@ window.dataLayer = window.dataLayer || [];
             options.target = '#' + $form_container.attr('id');
         }            
 
+        var formData = {};
+
         options.onFormSubmit = function( $form ){
+    	    
+    	    formData = $form.serializeArray().reduce( function( accu, item ) {
+    	       accu[item.name] = item.value;
+    	       return accu;
+    	    }, {});
 
-        	    var formData = $form.serializeArray().reduce( function( accu, item ) {
-        	       accu[item.name] = item.value;
-        	       return accu;
-        	    }, {});
-
-                if ( formData.hs_context )  {
-                    formData.hs_context = JSON.parse( formData.hs_context );
-                }
-        	    
-        	    dataLayer.push({
-                    'event': 'formSubmitted', 
-                    'formAction': $form_container.data('name'), 
-                    'formName': $form_container.data('name'),
-                    'formGuid': options.formId,
-                    'formElement': $form,
-                    'formData': formData
-                });
+            if ( formData.hs_context )  {
+                formData.hs_context = JSON.parse( formData.hs_context );
+            }
+    	    
+    	    dataLayer.push({
+                'event': 'formSubmit', 
+                'formAction': $form_container.data('name'), 
+                'formName': $form_container.data('name'),
+                'formGuid': options.formId,
+                'formElement': $form,
+                'formData': formData
+            });
         };  
+        
+        options.onFormSubmitted = function( $form ){
+    	    dataLayer.push({
+                'event': 'formSubmitted', 
+                'formAction': $form_container.data('name'), 
+                'formName': $form_container.data('name'),
+                'formGuid': options.formId,
+                'formData': formData
+            });
+        };          
 
         if ( hubspotFormThemeCss && $form_container.data('customCss') ) {
             options.onFormReady = function($form){ 
@@ -58,7 +64,7 @@ window.dataLayer = window.dataLayer || [];
         	    $form.addClass( $form_container.attr('class') );
         	};
         }
-
+        
         if ( options.portalId && options.formId ) {
             hbspt.forms.create(options);    
         }
